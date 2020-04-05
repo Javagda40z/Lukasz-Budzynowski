50e0026a0b42a3706ba1e747f99b41e2dd16dd06
Teno Eiss <lukasz12323@gmail.com>
2020-04-05 14:58:44 +0200

    eksperymenty na zwierzetach - abstrakcje

--- src/main/java/obiekty/Cat.java
+++ src/main/java/obiekty/Cat.java
@@ -2,12 +2,34 @@ package obiekty;
 
 public class Cat implements Animal{
 
+    private int licznik = 0;
+    private String imie;
+
+    public Cat(String imie) {
+        this.imie = imie;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
     public void drap() {
 //        kot drapie
+    }
+
+    public void eatMouse(){
+        licznik++;
+        System.out.println("Zjadłem " + licznik + " myszy");
+
     }
 
     @Override
     public void makeSound() {
         System.out.println("Miau!");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("chodzę");
     }
 }
