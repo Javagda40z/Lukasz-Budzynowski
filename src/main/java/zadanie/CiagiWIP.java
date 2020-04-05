d6fbfd998efceff07480e4b4a1a73c080c863833
Teno Eiss <lukasz12323@gmail.com>
2020-04-05 16:04:25 +0200

    veterynarz 2: optymalna praca

---
+++ src/main/java/zadanie/CiagiWIP.java
@@ -0,0 +1,28 @@
+package zadanie;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ciagi {
+
+    public static void main(String[] args) {
+
+    List<Integer> ciagNiearytmetyczny = Arrays.asList(1,2,3,4,2,2,3,6);
+        System.out.println(ciagNiearytmetyczny);
+
+        List<Integer> ciag = new LinkedList<>();
+
+
+
+        }
+
+
+
+
+
+
+        public static  boolean sprawdzCzyCiagJestArytmetyczny(List<Integer>ciag){
+
+    }
+}
